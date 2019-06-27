69b348a300985828e8ae7050e7999b833b6e6b7e
javierestrada26 <javiestrada14@hotmail.com>
2019-06-27 18:09:08 -0500

    diagrama

---
+++ EjemploClase/src/ejemploclase/Portatil.java
@@ -0,0 +1,25 @@
+
+package ejemploclase;
+
+/**
+ *
+ * @author javie
+ */
+public class Portatil {
+    String color ;
+    String procesador;
+    
+  public void establecerColor(String c){
+     color= c;
+  }
+  public  String  obtenercolor(){
+  return color;
+  }
+  public void establecerProcesador(String p){
+     procesador= p;
+  }
+  public  String  obtenerProcesador(){
+  return procesador;
+  }
+  
+}
